5809bd861f39608405b940fc78982b7baa2dc801
Sakina Shivji <shivjisakina@gmail.com>
2017-08-28 20:57:38 -0400

    set up files and initialized firebase

---
+++ train.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD2R5a0olG912Szr93ww1HgY83RDBztdOI",
+    authDomain: "train-schedular-8cda5.firebaseapp.com",
+    databaseURL: "https://train-schedular-8cda5.firebaseio.com",
+    projectId: "train-schedular-8cda5",
+    storageBucket: "",
+    messagingSenderId: "998004862453"
+};
+firebase.initializeApp(config);
